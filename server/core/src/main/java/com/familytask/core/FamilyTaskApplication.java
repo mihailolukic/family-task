0c3ffc2422e552833c2af63474fb87bfc4b099f7
mlukic <mlukic@smithmicro.com>
2022-04-23 14:11:15 +0200

    Initial server project setup

---
+++ server/core/src/main/java/com/familytask/core/FamilyTaskApplication.java
@@ -0,0 +1,13 @@
+package com.familytask.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTaskApplication.class, args);
+	}
+
+}
---
+++ server/core/src/test/java/com/familytask/core/FamilyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familytask.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
